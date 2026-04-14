d55ef62ffc20a33cecd19de94d73bd9a772112e8
Siva <namastereactsiva@gmail.com>
2026-04-14 20:33:56 +0530

    git file comment created

---
+++ gti.js
@@ -0,0 +1,41 @@
+/* 
+
+when you created a new file only if you add the 
+file then only we can commit and  push the file to git 
+
+
+to for that
+
+===> git add [file name] ---> this will only upload the what file you mention
+
+===> git add . ---> this will upload the all files
+
+*/
+
+/* 
+
+download the all latest code form the to use 
+
+===> git pull 
+
+before start the work download the code 
+
+*/
+
+/* 
+
+to save the all changes written use 
+
+===> git commit -m "[write the message]"
+
+this comment save the all code 
+
+*/
+
+/* 
+
+to update the all code changes you maded 
+
+===> git push 
+
+*/
