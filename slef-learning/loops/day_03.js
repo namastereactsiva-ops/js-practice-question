b456d3822abaf3c57fb6c31c3f194954f08127b2
siva <namastereactsiva@gmail.com>
2026-04-20 08:08:47 +0530

    Day 03 questions added

---
+++ slef-learning/loops/day_03.js
@@ -0,0 +1,15 @@
+// SECTION A: Basics (1–10)
+
+let arr = [10, 20, 30, 40, 50];
+
+// Print all elements of array
+
+// Print array elements in reverse order
+// Print first element
+// Print last element
+// Find sum of all elements
+// Find average of array elements
+// Count total elements in array (without using .length directly)
+// Print all elements at even index positions
+// Print all elements at odd index positions
+// Print only elements greater than 25
