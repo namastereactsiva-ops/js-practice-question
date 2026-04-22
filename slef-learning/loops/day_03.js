e4d7673a5b644417b4d1c7dd20e70ad05171ac98
siva <namastereactsiva@gmail.com>
2026-04-22 11:44:45 +0530

    day 03 completed

--- slef-learning/loops/day_03.js
+++ slef-learning/loops/day_03.js
@@ -80,7 +80,7 @@
 
 // SECTION B: Searching + Counting (11–20)
 
-let nums = [5, 2, 7, 2, 9, 2, 1, 7];
+// let nums = [5, 2, 7, 2, 9, 2, 1, 7];
 
 // Count how many times 2 appears
 
@@ -201,3 +201,135 @@ let nums = [5, 2, 7, 2, 9, 2, 1, 7];
 // }
 
 // console.log(newNum);
+
+// SECTION C: Advanced Array Logic (21–30)
+
+let data = [3, -2, 7, 0, -5, 10, -1, 8];
+
+// Count how many negative numbers exist
+
+// let count = 0;
+
+// for (let i = 0; i <= data.length - 1; i++) {
+//   if (data[i] < 0) {
+//     count++;
+//   }
+// }
+
+// console.log(count);
+
+// Count how many positive numbers exist
+
+// let count = 0;
+
+// for (let i = 0; i <= data.length - 1; i++) {
+//   if (data[i] > 0) {
+//     count++;
+//   }
+// }
+
+// console.log(count);
+
+// Count how many zeros exist
+
+// let count = 0;
+
+// for (let i = 0; i <= data.length - 1; i++) {
+//   if (data[i] === 0) {
+//     count++;
+//   }
+// }
+
+// console.log(count);
+
+// Print sum of positive numbers only
+
+// let sum = 0;
+
+// for (let i = 0; i <= data.length - 1; i++) {
+//   if (data[i] > 0) {
+//     sum += data[i];
+//   }
+// }
+
+// console.log(sum);
+
+// Print sum of negative numbers only
+
+// let sum = 0;
+
+// for (let i = 0; i <= data.length - 1; i++) {
+//   if (data[i] < 0) {
+//     sum += data[i];
+//   }
+// }
+
+// console.log(sum);
+
+// Create a new array without negative numbers
+
+// let newArray = [];
+
+// for (let i = 0; i <= data.length - 1; i++) {
+//   if (data[i] >= 0) {
+//     newArray.push(data[i]);
+//   }
+// }
+
+// console.log(newArray);
+
+// Reverse the array manually (don’t use .reverse())
+
+// let newArray = [];
+
+// for (let i = data.length - 1; i >= 0; i--) {
+//   newArray.push(data[i]);
+// }
+
+// console.log(newArray);
+
+// Remove all zeros and create a new array
+
+// let newArray = [];
+
+// for (let i = 0; i <= data.length - 1; i++) {
+//   if (data[i] === 0) {
+//     continue;
+//   } else {
+//     newArray.push(data[i]);
+//   }
+// }
+
+// console.log(newArray);
+
+// Find second largest number in the array
+
+// let secondBiggest = -Infinity;
+// let largenum = -Infinity;
+
+// for (let val of data) {
+//   if (val > largenum) {
+//     secondBiggest = largenum;
+//     largenum = val;
+//   } else if (val > secondBiggest && val !== largenum) {
+//     secondBiggest = val;
+//   }
+// }
+
+// console.log(secondBiggest);
+
+// Find second smallest number in the array
+
+// let secondSmall = Infinity;
+// let smallNum = Infinity;
+
+// for (let val of data) {
+//   if (val < smallNum) {
+//     secondSmall = smallNum;
+//     smallNum = val;
+//   } else if (val < secondSmall && val !== smallNum) {
+//     secondSmall = val;
+//   }
+// }
+
+// console.log(secondSmall);
